1f8d43098980291b64d15b5b6e5666cb82c7008e
Yacine Boudebouz <yacineboudebbouz7@gmail.com>
2023-10-30 12:50:50 +0100

    orders

--- lib/src/features/checkout/presentation/payment/payment_page.dart
+++ lib/src/features/checkout/presentation/payment/payment_page.dart
@@ -1,41 +1,40 @@
+import 'package:ecommerce_app/src/common_widgets/async_value_widget.dart';
+import 'package:ecommerce_app/src/features/cart/application/cart_service.dart';
+import 'package:ecommerce_app/src/features/cart/domain/cart.dart';
 import 'package:ecommerce_app/src/features/cart/presentation/shopping_cart/shopping_cart_item.dart';
 import 'package:ecommerce_app/src/features/cart/presentation/shopping_cart/shopping_cart_items_builder.dart';
 import 'package:ecommerce_app/src/features/checkout/presentation/payment/payment_button.dart';
 import 'package:ecommerce_app/src/features/cart/domain/item.dart';
+import 'package:ecommerce_app/src/routing/app_router.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 
 /// Payment screen showing the items in the cart (with read-only quantities) and
 /// a button to checkout.
-class PaymentPage extends StatelessWidget {
+class PaymentPage extends ConsumerWidget {
   const PaymentPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    // TODO: Listen to cart changes on checkout and update the UI.
-    // TODO: Read from data source
-    const cartItemsList = [
-      Item(
-        productId: '1',
-        quantity: 1,
-      ),
-      Item(
-        productId: '2',
-        quantity: 2,
-      ),
-      Item(
-        productId: '3',
-        quantity: 3,
-      ),
-    ];
-
-    return ShoppingCartItemsBuilder(
-      items: cartItemsList,
-      itemBuilder: (_, item, index) => ShoppingCartItem(
-        item: item,
-        itemIndex: index,
-        isEditable: false,
-      ),
-      ctaBuilder: (_) => const PaymentButton(),
-    );
+  Widget build(BuildContext context, WidgetRef ref) {
+    ref.listen<double>(cartTotalProvider, (previous, cartTotal) {
+      if (cartTotal == 0.0) {
+        context.goNamed(AppRoute.orders.name);
+      }
+    });
+    final cartValue = ref.watch(cartProvider);
+    return AsyncValueWidget(
+        value: cartValue,
+        data: (data) {
+          return ShoppingCartItemsBuilder(
+            items: data.toItemsList(),
+            itemBuilder: (_, item, index) => ShoppingCartItem(
+              item: item,
+              itemIndex: index,
+              isEditable: false,
+            ),
+            ctaBuilder: (_) => const PaymentButton(),
+          );
+        });
   }
 }
