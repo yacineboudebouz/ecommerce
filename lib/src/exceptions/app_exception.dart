9e5515425ce00ac0440c99649d53a74dd76e4b17
Yacine Boudebouz <yacineboudebbouz7@gmail.com>
2024-02-09 18:27:36 +0100

    error handling

---
+++ lib/src/exceptions/app_exception.dart
@@ -0,0 +1,13 @@
+
+
+// import 'package:freezed_annotation/freezed_annotation.dart';
+
+// part 'app_exception_freezed.dart';
+
+// @freezed
+// class AppException with _$AppException {
+//   const factory AppException.emailAlreadyInUse() = EmailALreadyInUse;
+  
+  
+
+// }
